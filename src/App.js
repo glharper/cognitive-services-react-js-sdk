4960f757ecdc75e5c6c8dbd3467edd90dcb2dabe
Glenn Harper <glharper@microsoft.com>
2021-01-08 10:19:21 -0800

    Finish form refactoring to react idiomatic

--- src/App.js
+++ src/App.js
@@ -62,99 +62,125 @@ const KeyForm = (props) => {
     );
 }
 
+const LanguageOptions = (props) => {
+    const languageOptions = [
+      {val:"en-US", name:"English - US"},
+      {val:"ar-EG", name:"Arabic - EG"},
+      {val:"ca-ES", name:"Catalan - ES"},
+      {val:"da-DK", name:"Danish - DK"},
+      {val:"da-DK", name:"Danish - DK"},
+      {val:"de-DE", name:"German - DE"},
+      {val:"en-AU", name:"English - AU"},
+      {val:"en-CA", name:"English - CA"},
+      {val:"en-GB", name:"English - GB"},
+      {val:"en-IN", name:"English - IN"},
+      {val:"en-NZ", name:"English - NZ"},
+      {val:"es-ES", name:"Spanish - ES"},
+      {val:"es-MX", name:"Spanish - MX"},
+      {val:"fi-FI", name:"Finnish - FI"},
+      {val:"fr-CA", name:"French - CA"},
+      {val:"fr-FR", name:"French - FR"},
+      {val:"hi-IN", name:"Hindi - IN"},
+      {val:"it-IT", name:"Italian - IT"},
+      {val:"ja-JP", name:"Japanese - JP"},
+      {val:"ko-KR", name:"Korean - KR"},
+      {val:"nb-NO", name:"Norwegian - NO"},
+      {val:"nl-NL", name:"Dutch - NL"},
+      {val:"pl-PL", name:"Polish - PL"},
+      {val:"pt-BR", name:"Portuguese - BR"},
+      {val:"pt-PT", name:"Portuguese - PT"},
+      {val:"ru-RU", name:"Russian - RU"},
+      {val:"sv-SE", name:"Swedish - SE"},
+      {val:"zh-CN", name:"Chinese - CN"},
+      {val:"zh-HK", name:"Chinese - HK"},
+      {val:"zh-TW", name:"Chinese - TW"}
+    ];
+    return (
+      <>
+        {languageOptions.map(opt => <option value={opt.val}>{opt.name}</option>)}
+      </>
+    );
+}
+
+const RegionOptions = (props) => {
+    const regionOptions = [
+      {val:"westus", name:"West US"},
+      {val:"westus2", name:"West US2"},
+      {val:"eastus", name:"East US"},
+      {val:"eastus2", name:"East US2"},
+      {val:"eastasia", name:"East Asia"},
+      {val:"southeastasia", name:"South East Asia"},
+      {val:"northeurope", name:"North Europe"},
+      {val:"westeurope", name:"West Europe"}
+    ];
+    return (
+      <>
+        {regionOptions.map(opt => <option value={opt.val}>{opt.name}</option>)}
+      </>
+    );
+}
+
+const SelectForm = ({ title, value, onSelect, children }) => {
+    return (
+          <tr>
+              <td align="right">{title}:</td>
+              <td align="left">
+                  <select
+                    value={value}
+                    onChange={event => onSelect(event.target.value)}
+                  >
+                    {children}
+                  </select>
+              </td>
+          </tr>
+    );
+}
+
+const RecognitionButtons = ({ onStart, onStop, recognizing }) => {
+    return (
+          <tr>
+              <td align="left"><b>Speech SDK API recognizeOnceAsync:</b></td>
+              <td>
+                  <button id="speechsdkStartRecognizeOnceAsync" disabled={recognizing.toString()==="true" ? "disabled": ""} onClick={onStart}>recognizeOnceAsync()</button>
+                  <button id="speechsdkStopRecognizeOnceAsync" disabled={recognizing.toString()==="false" ? "disabled": ""} onClick={onStop}>STOP recognizeOnceAsync()</button>
+              </td>
+          </tr>
+    );
+}
+
 class SpeechTable extends Component {
   constructor(props) {
     super(props);
-    this.state = {results: "", events: "", subscriptionKey: "YOUR_SPEECH_API_KEY", recognizing: false};
+    this.state = {results: "", events: "", subscriptionKey: "YOUR_SPEECH_API_KEY", recognizing: false, language: "en-US", region:"westus"};
     this.eventStyle = {display: "inline-block",width:"500px",height:"200px",overflow: "scroll",whiteSpace: "nowrap"};
     this.resultStyle = {display: "inline-block", width:"500px", height:"200px"};
   }
 
   updateKey = value => this.setState({ subscriptionKey: value });
-  startRecognition = () => this.setState({ recognizing: true });
-  endRecognition = () => this.setState({ recognizing: false });
+  updateLanguage = value => this.setState({ language: value });
+  updateRegion = value => this.setState({ region: value });
+  startRecognition = () => {
+    this.setState({ recognizing: true });
+    //hook speech recognition in here
+  };
+  endRecognition = () => {
+    this.setState({ recognizing: false });
+  };
   render () {
     return (
       <table>
         <Title />
         <KeyForm value={this.state.subscriptionKey} onChange={this.updateKey} />
-          <tr>
-              <td align="right">Language:</td>
-              <td align="left">
-                  <select id="languageOptions">
-                      <option value="ar-EG">Arabic - EG</option>
-                      <option value="ca-ES">Catalan - ES</option>
-                      <option value="da-DK">Danish - DK</option>
-                      <option value="da-DK">Danish - DK</option>
-                      <option value="de-DE">German - DE</option>
-                      <option value="en-AU">English - AU</option>
-                      <option value="en-CA">English - CA</option>
-                      <option value="en-GB">English - GB</option>
-                      <option value="en-IN">English - IN</option>
-                      <option value="en-NZ">English - NZ</option>
-                      <option value="en-US" selected="selected">English - US</option>
-                      <option value="es-ES">Spanish - ES</option>
-                      <option value="es-MX">Spanish - MX</option>
-                      <option value="fi-FI">Finnish - FI</option>
-                      <option value="fr-CA">French - CA</option>
-                      <option value="fr-FR">French - FR</option>
-                      <option value="hi-IN">Hindi - IN</option>
-                      <option value="it-IT">Italian - IT</option>
-                      <option value="ja-JP">Japanese - JP</option>
-                      <option value="ko-KR">Korean - KR</option>
-                      <option value="nb-NO">Norwegian - NO</option>
-                      <option value="nl-NL">Dutch - NL</option>
-                      <option value="pl-PL">Polish - PL</option>
-                      <option value="pt-BR">Portuguese - BR</option>
-                      <option value="pt-PT">Portuguese - PT</option>
-                      <option value="ru-RU">Russian - RU</option>
-                      <option value="sv-SE">Swedish - SE</option>
-                      <option value="zh-CN">Chinese - CN</option>
-                      <option value="zh-HK">Chinese - HK</option>
-                      <option value="zh-TW">Chinese - TW</option>
-                  </select>
-              </td>
-          </tr>
-          <tr>
-              <td align="right">Region:</td>
-              <td align="left">
-                  <select id="regionOptions">
-                      <option value="westus" selected="selected">West US</option>
-                      <option value="westus2">West US2</option>
-                      <option value="eastus">East US</option>
-                      <option value="eastus2">East US2</option>
-                      <option value="eastasia">East Asia</option>
-                      <option value="southeastasia">South East Asia</option>
-                      <option value="northeurope">North Europe</option>
-                      <option value="westeurope">West Europe</option>
-                  </select>
-              </td>
-          </tr>
-          <tr>
-              <td align="right">Input:</td>
-              <td align="left">
-                  <select id="inputSource">
-                      <option value="Mic" selected="selected">Microphone</option>
-                      <option value="File">Audio File</option>
-                  </select>
-              </td>
-          </tr>
-          <tr>
-              <td></td>
-              <td>
-                  <input type="file" id="filePicker" accept=".wav" style={{display:"none"}} />
-              </td>
-          </tr>
-          <tr>
-              <td align="left"><b>Speech SDK API recognizeOnceAsync:</b></td>
-              <td>
-                  <button id="speechsdkStartRecognizeOnceAsync">recognizeOnceAsync()</button>
-                  <button id="speechsdkStopRecognizeOnceAsync" disabled="disabled">STOP recognizeOnceAsync()</button>
-              </td>
-          </tr>
-          
-          <ResultForm title="Results:" text={this.state.results} style={this.resultStyle}/>
-          <ResultForm title="Events:" text={this.state.events} style={this.eventStyle}/>
+        <SelectForm title={"Language"} value={this.state.language} onSelect={this.updateLanguage}>
+          <LanguageOptions />
+        </SelectForm>
+        <SelectForm title={"Region"} value={this.state.region} onSelect={this.updateRegion}>
+          <RegionOptions />
+        </SelectForm>
+        <RecognitionButtons recognizing={this.state.recognizing} onStart={this.startRecognition} onStop={this.endRecognition}/>
+        
+        <ResultForm title="Results:" text={this.state.results} style={this.resultStyle}/>
+        <ResultForm title="Events:" text={this.state.events} style={this.eventStyle}/>
       </table>
     );
   }
@@ -165,18 +191,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <SpeechTable />
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
